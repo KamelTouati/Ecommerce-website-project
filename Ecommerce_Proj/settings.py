62267de3533b2ca7651cb5f19b3df9cc28af6c77
KamelTouati <kameltouati19.work@gmail.com>
2023-02-14 17:31:03 +0100

    add font-awsome & tailwindcss & create store page

--- Ecommerce_Proj/settings.py
+++ Ecommerce_Proj/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+# STATIC_DIRS = [
+#     # os.path.join(BASE_DIR, 'static')
+#     BASE_DIR / "Ecommerce_Proj/static",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- store/views.py
+++ store/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def store(request):
+    context = {}
+    return render (request, 'store/pages/store.html', context)
+
+def cart(request):
+    context = {}
+    return render (request, 'store/pages/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render (request, 'store/pages/checkout.html', context)
